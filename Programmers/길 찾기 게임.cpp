4c12793221ab875b487a081a6aea7b1ba44df0f2
Domo Lee <edh1021@gmail.com>
2020-05-22 22:51:08 +0900

    5/22

--- Programmers/길 찾기 게임.cpp
+++ Programmers/길 찾기 게임.cpp
@@ -1,30 +1,105 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <map>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-bool compare(const vector<int> a, const vector<int> b)
-{
-	if (a[1] == b[1])
-		return a[0] < b[0];
-	return a[1] > b[1];
+
+vector<vector<int>> answer;
+
+typedef struct node {
+	int num;
+	int x;
+	struct node *left;
+	struct node *right;
+};
+typedef struct qnode {
+	int x;
+	int y;
+	int num;
+};
+
+struct cmp {
+	bool operator()(qnode a, qnode b) {
+		if (a.y == b.y)
+			return a.x > b.x;
+		return a.y < b.y;
+	}
+};
+
+node *root;
+
+void find(node *curr, node *newNode) {
+
+	if (curr->x > newNode->x && curr->left != NULL) { //현재 노드보다 왼쪽인데 이미 왼쪽 자식 노드 존재
+		find(curr->left, newNode);
+	}
+	else if (curr->x > newNode->x && curr->left == NULL) {
+		curr->left = newNode;
+	}
+	else if (curr->x < newNode->x && curr->right != NULL) {
+		find(curr->right, newNode);
+	}
+	else {
+		curr->right = newNode;
+	}
+}
+
+void preorder(node *curr) {
+	if (curr == NULL)
+		return;
+	answer[0].push_back(curr->num);
+	preorder(curr->left);
+	preorder(curr->right);
+}
+void postorder(node *curr) {
+	if (curr == NULL)
+		return;
+	postorder(curr->left);
+	postorder(curr->right);
+	answer[1].push_back(curr->num);
 }
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
-	vector<vector<int>> answer;
 
-	map<pair<int, int>, int> values;
+	priority_queue< qnode, vector<qnode>, cmp> nodes;
+	//y값이 클수록 x값이 작을수록 우선순위가 되도록
+	qnode input;
+	answer.resize(2);
 
-	for (int i = 0; i < nodeinfo.size(); i++)
-		values[make_pair(nodeinfo[i][0], nodeinfo[i][1])] = i+1;
+	for (int i = 0; i<nodeinfo.size(); i++) {
+		input.x = nodeinfo[i][0];
+		input.y = nodeinfo[i][1];
+		input.num = i + 1;
+		nodes.push(input);
+	}
 
-	sort(nodeinfo.begin(), nodeinfo.end(), compare);
+	qnode curr, before;
+	node *newNode, *tmp;
+
+	newNode = (node *)malloc(sizeof(node));
+	curr = nodes.top();
+	nodes.pop();
+	newNode->num = curr.num;
+	newNode->x = curr.x;
+	newNode->left = newNode->right = NULL;
+	root = newNode;
+
+	while (!nodes.empty()) {
+		curr = nodes.top();
+		nodes.pop();
+		newNode = (node *)malloc(sizeof(node));
+		newNode->num = curr.num;
+		newNode->x = curr.x;
+		newNode->left = newNode->right = NULL;
+		find(root, newNode);
+	}
+
+	preorder(root);
+	postorder(root);
 
 	return answer;
 }
-
 void main()
 {
 	solution({ {5,3},{11,5},{13,3},{3,5 },{6,1},{1,3},{8,6},{7,2},{2,2} });
