48905aed784da99c56f17b88cb5c5fd3f6d78847
Domo Lee <edh1021@gmail.com>
2020-07-04 00:11:04 +0900

    7/4

--- BOJ/16398.cpp
+++ BOJ/16398.cpp
@@ -4,10 +4,26 @@
 		풀이 일자 : 2020.07.02
 
 	[풀이 방법] : 최소 신장 트리 (프림 알고리즘)
+		1. 프림 알고리즘을 이용해 현재 노드의 인접 노드 중 가중치가 작은 간선을 선택한다.
+		2. isUsed를 사용해 이미 방문한 노드는 재방문 하지 않는다.
 */
 
 #include <iostream>
 #include <vector>
+#include <queue>
+
+struct node {
+	int cost;
+	int index;
+};
+
+struct cmp
+{
+	bool operator()(const node& a, const node& b)
+	{
+		return a.cost > b.cost;
+	}
+};
 
 int main()
 {
@@ -16,8 +32,9 @@ int main()
 
 	int N;
 	std::cin >> N;
-	
-	std::vector<std::vector<std::pair<int, int>>> graph (N,std::vector<std::pair<int,int>>(N));
+
+	std::vector<std::vector<std::pair<int, int>>> graph(N, std::vector<std::pair<int, int>>(N));
+	std::vector<bool> isUsed(N, false);
 
 	for (int i = 0; i < N; i++)
 	{
@@ -29,6 +46,27 @@ int main()
 		}
 	}
 
-	std::cout << "test";
+	std::priority_queue<node, std::vector<node>, cmp> queue;
+	queue.push({ 0,0});
+	int answer = 0;
+	while (!queue.empty())
+	{
+		auto top = queue.top();
+		queue.pop();
+		const int cost = top.cost;
+		const int index = top.index;
+
+		if (isUsed[index]) continue;
+		isUsed[index] = true;
+		answer +=cost;
+		for (const auto& i : graph[index])
+		{
+			if (isUsed[i.second]) continue;
+			if (i.first == 0) continue;
+			queue.push({ i.first,i.second });
+
+		}
+	}
+	std::cout << answer;
 	return 0;
 }
