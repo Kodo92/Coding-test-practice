766b9b8ae82456d138a38fd4aa8aceeae5fd1217
Domo Lee <edh1021@gmail.com>
2020-07-08 23:46:06 +0900

    2회차 풀이

--- Programmers/조이스틱.cpp
+++ Programmers/조이스틱.cpp
@@ -118,6 +118,73 @@ int solution(string name)
 }
 */
 
+// 2»∏ «Æ¿Ã
+/*
+int changeChar(string& name, string& str, const int index)
+{
+	int upCount = name[index] - 'A';
+	int downCount = 'Z' + 1 - name[index];
+	swap(str[index], name[index]);
+	return min(upCount, downCount);
+}
+
+int getIndex(const string& name, const string& str, const int nowPos, int* destPos)
+{
+	int leftCount = 1, leftIndex = nowPos - 1, rightCount = 1, rightIndex = nowPos + 1;
+	if (leftIndex < 0)
+		leftIndex += str.size();
+	if (rightIndex >= str.size())
+		rightIndex %= str.size();
+
+	while (name[leftIndex] == 'A')
+	{
+		leftIndex--;
+		leftCount++;
+		if (leftIndex < 0)
+			leftIndex += str.size();
+	}
+	while (name[rightIndex] == 'A')
+	{
+		rightIndex++;
+		rightCount++;
+		if (rightIndex >= str.size())
+			rightIndex %= str.size();
+	}
+	if (leftCount < rightCount)
+	{
+		*destPos = leftIndex;
+		return leftCount;
+	}
+	else
+	{
+		*destPos = rightIndex;
+		return rightCount;
+	}
+}
+
+int solution(string name) {
+
+	string str;
+
+	for (int i = 0; i < name.size(); i++)
+		str.push_back('A');
+	string tempStr = str;
+	int nowPos = 0, destPos = 0;
+	int answer = 0;
+
+	if (name[0] != 'A')
+		answer += changeChar(name, str, 0);
+
+	while (tempStr != name)
+	{
+		answer += getIndex(name, str, nowPos, &destPos);
+		answer += changeChar(name, str, destPos);
+		nowPos = destPos;
+	}
+
+	return answer;
+}
+*/
 int solution(string name)
 {
 	int answer = 0;
