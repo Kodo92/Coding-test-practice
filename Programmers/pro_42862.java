59a9a708795a20b11e6ca027ca9d4cea7ac9e958
LeeDongHo <edh1021@gmail.com>
2021-04-07 17:01:03 +0900

    약수 구하기

---
+++ Programmers/pro_42862.java
@@ -0,0 +1,11 @@
+public class pro_42862 {
+    public static void main(String[] args) {
+        System.out.println(solution(5,new int[] {2,4}, new int[] {1,3,5}));
+    }
+
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+
+        return answer;
+    }
+}
