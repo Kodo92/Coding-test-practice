8e20b235588b9466427771a850a51e8489b3c5d5
LeeDongHo <edh1021@gmail.com>
2021-04-08 23:28:28 +0900

    체육복

--- Programmers/pro_42862.java
+++ Programmers/pro_42862.java
@@ -1,11 +1,32 @@
 public class pro_42862 {
     public static void main(String[] args) {
-        System.out.println(solution(5,new int[] {2,4}, new int[] {1,3,5}));
+        System.out.println(solution(5,new int[] {3}, new int[] {1}));
     }
 
     public static int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+        int[] student = new int[n];
+        for(int i : lost) {
+            student[i-1] -= 1;
+        }
+        for(int i : reserve) {
+            student[i-1] += 1;
+        }
 
+        for(int i = 0; i<n ; i++) {
+            if(student[i] < 1) continue;
+            if(i-1 >= 0 && student[i-1] < 0) {
+                student[i-1] += 1;
+                student[i] -= 1;
+            } else if(i+1 < n && student[i+1] < 0) {
+                student[i+1] += 1;
+                student[i] -= 1;
+            }
+        }
+        int answer = 0;
+        for(int i : student) {
+            if(i >= 0)
+                answer++;
+        }
         return answer;
     }
 }
